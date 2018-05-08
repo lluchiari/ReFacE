0bff85af1163715a288325ea520b3276ca2c5a8e
Lucas Luchiari <luchiari.lucas@gmail.com>
2018-05-08 11:15:05 -0300

    Adding new config files and teste the framework

---
+++ build/lib/ReFacE/mains.py
@@ -0,0 +1,6 @@
+
+def main():
+    print(f"Oi, I've just started!!!")
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
+import os
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 config = {
     'description': 'This project is my graduation project in computer vision. It\'s a system that recognizes users\'s face in a database.',
+    'long_description':read('README'),
     'author': 'Lucas Luchiari',
     'url': 'https://github.com/lluchiari/ReFacE',
     'author_email': 'luchiari.lucas@gmail.com',
